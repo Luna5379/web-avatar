4f707c123863b0de3ea3e2901dbe56a3abb6ad71
Luna5379 <110072210+Luna5379@users.noreply.github.com>
2024-07-30 16:53:07 +0100

    Create hair.js

---
+++ hair.js
@@ -0,0 +1,7 @@
+function changeHairstyle() {
+  document.getElementById("hair").addEventListener("click", hairstyle);
+  function hairstyle() {
+    let hairstyle = "bob":
+    let color = #99740e;
+  }
+}
